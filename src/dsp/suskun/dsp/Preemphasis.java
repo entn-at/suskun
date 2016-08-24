ece36068f538ec8fa2315cefa0d421a92ab30f7a
ahmetaa <ahmetaa@gmail.com>
2016-08-24 22:56:29 +0300

    name change

--- src/dsp/suskun/dsp/Preemphasis.java
+++ src/dsp/suskun/dsp/Preemphasis.java
@@ -4,15 +4,17 @@ import suskun.core.FloatData;
 import suskun.core.FloatDataProcessor;
 
 /**
- * This is a simple preemphasizer. Applies the formula of x[1] = x1[1] - x[0] * coefficient.
- * For x[0], x[0] = x[0] - x[0] * coefficient
- * This is an in-place operation, so that input data will be modified after processing.
+ * This applies preemphasis to input with the
+ * formula x[1] = x1[1] - x[0] * coefficient from 1..n
+ * x[0] = x[0] - x[0] * coefficient for 0
+ * Process is in-place, so that input data will be modified after processing.
+ * This is a stateless preemphasis function. 
  */
-public class Preemphasizer implements FloatDataProcessor {
+public class Preemphasis implements FloatDataProcessor {
 
     float coefficient;
 
-    public Preemphasizer(float coefficient) {
+    public Preemphasis(float coefficient) {
         this.coefficient = coefficient;
     }
 
